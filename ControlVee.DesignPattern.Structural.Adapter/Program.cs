64058da29b61c6777772abf1f1ff195d2251d980
controlvee <adam.goerlich@gmail.com>
2021-02-20 23:01:49 -0600

    .

--- ControlVee.DesignPattern.Structural.Adapter/Program.cs
+++ ControlVee.DesignPattern.Structural.Adapter/Program.cs
@@ -7,9 +7,26 @@ namespace ControlVee.DesignPattern.Structural.Adapter
     /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// 
+        /// </summary>
+        static void Main()
         {
-            
+            // Non-adapted chemical compound.
+            Compound unknown = new Compound("Unknown");
+            unknown.Display();
+
+            // Adapted chemical compounds.
+            Compound water = new RichCompound("Water");
+            water.Display();
+
+            Compound benzene = new RichCompound("Benzene");
+            benzene.Display();
+
+            Compound ethanol = new RichCompound("Ethanol");
+            ethanol.Display();
+
+            Console.ReadKey();
         }
     }
 
